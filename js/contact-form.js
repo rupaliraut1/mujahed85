24b8ac3c4d186f79860a68f93a0878a6ae588758
ravib7 <ravib7@gmail.com>
2024-01-22 15:55:43 +0530

    added

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,36 +1,35 @@
-// Get form ID from the HTML document
-const formId = 'contact-form'; // Change this to your actual form ID
+function postToGoogle() {
+    // var field1 = $("input[type='text'][name='name']").val();
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var message = $('#message').val();
 
-// Attach event listener to the form
-document.getElementById(formId).addEventListener('submit', function (e) {
-    e.preventDefault(); // Prevent the default form submission
+    $.ajax({
+      url: "https://docs.google.com/forms/d/e/1FAIpQLSdjOTKRb7YiWi8OGPq6M6CRL0TpuAsUKacKp2XgruMbIp4wzg/formResponse",
+      data: {
+        "entry.924752166": name,
+        "entry.997497831": email,
 
-    // Collect form data
-    const formData = new FormData(e.target);
-
-    // Convert FormData to an object
-    const formDataObject = {};
-    formData.forEach((value, key) => {
-        formDataObject[key] = value;
-    });
-
-    // Replace 'yourFormKey' with the actual key from your Google Form URL
-    const googleFormURL = 'https://docs.google.com/forms/d/e/1FAIpQLSfa2rF0fv7nPpP_6WKl0ryXkAG4SGYjFv3dlLKvnZPRlme0lQ/viewform?usp=sf_link';
-
-    // Send data to Google Forms using Fetch API
-    fetch(googleFormURL, {
-        method: 'POST',
-        body: new URLSearchParams(formDataObject),
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function() {
+          //Success message
+        },
+        200: function() {
+          //Success Message
         }
-    })
-    .then(response => {
-        // Handle the response (e.g., redirect or show a success message)
-        console.log('Form submission successful:', response);
-    })
-    .catch(error => {
-        // Handle errors
-        console.error('Error submitting form:', error);
+      }
     });
-});
+  }
+ 
+ /* 
+ $(document).ready(function() {
+    $('#form').submit(function() {
+      postToGoogle();
+      return false;
+    });
+    
+ });
+ */
