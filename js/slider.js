057e6f77be211d74d255e59b14f6ebbd3d632ac7
ravib7 <ravib7@gmail.com>
2024-01-24 15:02:47 +0530

    footer correction

--- js/slider.js
+++ js/slider.js
@@ -14,19 +14,21 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
   let captionText = document.getElementById("caption");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].styles.display = "block";
-  dots[slideIndex - 1].className += " active";
-  //captionText.innerHTML = dots[slideIndex-1].alt;
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 
-  // Auto play after 3 seconds
+
+
+//   // Auto play after 3 seconds
   setTimeout(function () {
     plusSlides(1);
   }, 3000);
