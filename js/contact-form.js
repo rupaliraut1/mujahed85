c99faa1ef0c0ebf4b2a623a128d1e8ae5490111a
ravib7 <ravib7@gmail.com>
2024-01-27 16:43:28 +0530

    contact

--- js/contact-form.js
+++ js/contact-form.js
@@ -4,7 +4,7 @@ function submitForm() {
     var message = document.getElementById('message').value;
 
     // Replace 'YOUR_GOOGLE_FORM_URL' with your actual Google Form URL
-    var googleFormUrl = 'https://script.google.com/macros/s/AKfycbzdxpGy9GYKsYN2-4oX7wepl8gGjsqBNEoU_zO0ajcda42gjWkiImzA9YBDWCR7s42kSA/exec';
+    var googleFormUrl = 'https://script.google.com/macros/s/AKfycbzMKVBB9wvuciu6qPPYg8P6fTMidemmUJAAMQtsdXcAqYr_IXqSSJHEktCZcumtjanf/exec';
 
     var formData = new FormData();
     formData.append('entry.1439917167', name);
