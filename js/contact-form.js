f26141b7be66c36c77c34f227d3b35b047814936
ravib7 <ravib7@gmail.com>
2024-01-27 13:33:28 +0530

    updated contact page

--- js/contact-form.js
+++ js/contact-form.js
@@ -4,12 +4,12 @@ function submitForm() {
     var message = document.getElementById('message').value;
 
     // Replace 'YOUR_GOOGLE_FORM_URL' with your actual Google Form URL
-    var googleFormUrl = 'https://script.google.com/macros/s/AKfycbzuT1QeVN6kNbTB1e8QDt-vDdme0yK4tzy-vRSo-ThFqxN_5f5Lyl0FTD39RAz2UsSv2g/exec';
+    var googleFormUrl = 'https://script.google.com/macros/s/AKfycbztLSX2FThihgGsPmXRyELBTHrftiMZSarwsWAAHuTTIo-tx_tSxky6qeVGFkMDig2_Jg/exec';
 
     var formData = new FormData();
-    formData.append('entry.1777579499', name);
-    formData.append('entry.878578141', email);
-    formData.append('entry.992318106', message);
+    formData.append('entry.1439917167', name);
+    formData.append('entry.5816732', email);
+    formData.append('entry.2054302966', message);
 
     fetch(googleFormUrl, {
         method: 'POST',
