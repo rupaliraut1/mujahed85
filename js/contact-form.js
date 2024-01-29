a7e02187a0dd50027a2d97d2613f7866e3a06023
ravib7 <ravib7@gmail.com>
2024-01-29 15:57:31 +0530

    updated contact.js and contact.html

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,25 +1,40 @@
-function submitForm() {
+function submitForm(event) {
+    event.preventDefault();  // Prevent the default form submission
+    
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
 
     // Replace 'YOUR_GOOGLE_FORM_URL' with your actual Google Form URL
-    var googleFormUrl = 'https://script.google.com/macros/s/AKfycbzMKVBB9wvuciu6qPPYg8P6fTMidemmUJAAMQtsdXcAqYr_IXqSSJHEktCZcumtjanf/exec';
+    var googleFormUrl = 'https://script.google.com/macros/s/AKfycbyDJuWfeSSENytZxOwfg_T3R7e71tfkhj1mdaNJVqnTQnmzQPZgKj2Vdngyum4ICLsovw/exec';
 
     var formData = new FormData();
     formData.append('entry.1439917167', name);
     formData.append('entry.5816732', email);
     formData.append('entry.2054302966', message);
+
     fetch(googleFormUrl, {
         method: 'POST',
         body: formData,
     })
     .then(response => {
-        //console.log('Form submitted successfully:', response);
-        alert("Form submitted successfully!");
+        if (response.ok) {
+            console.log('Form submitted successfully:', response);
+
+            // Clear the form values after successful submission
+            document.getElementById('name').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('message').value = '';
+
+            // Display success message on the page
+            alert('Form submitted successfully! Thank you.');
+        } else {
+            console.error('Error submitting form. Server responded with:', response.status);
+            alert('Error submitting form. Please try again later.');
+        }
     })
     .catch(error => {
-        //console.error('Error submitting form:', error);
-        alert("Error submitting form. Please try again later.");
+        console.error('Error submitting form:', error);
+        alert('Error submitting form. Please try again later.');
     });
 }
