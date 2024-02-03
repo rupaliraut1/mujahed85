1fc8d97d5e7e03943f1d617819e3bdd68bfcdeb7
majed <khanmajed8857@gmail.com>
2024-02-03 21:35:07 +0530

    new updates

--- portfolio/assets/js/Contact.js
+++ portfolio/assets/js/Contact.js
@@ -1,37 +1,38 @@
 function submitForm(event) {
-  event.preventDefault();  // Prevent the default form submission
+    event.preventDefault();  // Prevent the default form submission
+    
+    var name = document.getElementById('name').value;
+    var message = document.getElementById('message').value;
   
-  var name = document.getElementById('name').value;
-  var message = document.getElementById('message').value;
-
-  // Replace 'YOUR_GOOGLE_FORM_URL' with your actual Google Form URL
-  var googleFormUrl = 'https://script.google.com/macros/s/AKfycbzwgU8xQzHlsuwmWKlO6e90aXABwuyMbVc9YMQd994I4i7Wnoykuq_ZZNQdU4zBmGPDAA/exec';
-
-  var formData = new FormData();
-  formData.append('entry.617381392', name);
-  formData.append('entry.1039351751', message);
-
-  fetch(googleFormUrl, {
-      method: 'POST',
-      body: formData,
-  })
-  .then(response => {
-      if (response.ok) {
-          console.log('Form submitted successfully:', response);
-
-          // Clear the form values after successful submission
-          document.getElementById('name').value = '';
-          document.getElementById('message').value = '';
-
-          // Display success message on the page
-          alert('Form submitted successfully! Thank you.');
-      } else {
-          console.error('Error submitting form. Server responded with:', response.status);
-          alert('Error submitting form. Please try again later.');
-      }
-  })
-  .catch(error => {
-      console.error('Error submitting form:', error);
-      alert('Error submitting form. Please try again later.');
-  });
-}
+    // Replace 'YOUR_GOOGLE_FORM_URL' with your actual Google Form URL
+    var googleFormUrl = 'https://script.google.com/macros/s/AKfycbzwgU8xQzHlsuwmWKlO6e90aXABwuyMbVc9YMQd994I4i7Wnoykuq_ZZNQdU4zBmGPDAA/exec';
+  
+    var formData = new FormData();
+    formData.append('entry.617381392', name);
+    formData.append('entry.1039351751', message);
+  
+    fetch(googleFormUrl, {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Form submitted successfully:', response);
+  
+            // Clear the form values after successful submission
+            document.getElementById('name').value = '';
+            document.getElementById('message').value = '';
+  
+            // Display success message on the page
+            alert('Form submitted successfully! Thank you.');
+        } else {
+            console.error('Error submitting form. Server responded with:', response.status);
+            alert('Error submitting form. Please try again later.');
+        }
+    })
+    .catch(error => {
+        console.error('Error submitting form:', error);
+        alert('Error submitting form. Please try again later.');
+    });
+  }
+  
